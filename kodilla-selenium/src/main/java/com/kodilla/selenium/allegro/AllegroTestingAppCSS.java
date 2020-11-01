009bf8b2ecf76fd9926b3fb2bb599d951b89f0be
Izabela Gajewska <izabela.gajewska93@outlook.com>
2020-11-01 17:56:26 +0100

    13.5

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingAppCSS.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingAppCSS.java
@@ -4,9 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public class AllegroTestingAppCSS {
     static WebDriver driver;
@@ -14,28 +13,29 @@ public class AllegroTestingAppCSS {
     public static void main(String[] args) {
 
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
+        WebDriver webDriver = new ChromeDriver();
         driver.get("https://www.allegro.pl");
-        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(By.cssSelector("img[alt='zamknij']"))).click();
 
-        Search("Elektronika", "mavic mini");
+        WebElement webElement = driver.findElement(By.cssSelector("button[class='_13q9y _8hkto munh_56_m m7er_k4 m7er_wn m7er_56_m']"));
+        webElement.click();
+        WebElement input = driver.findElement(By.cssSelector("input[type='search']"));
+        input.sendKeys("Mavic mini");
+        WebElement eleSelect = driver.findElement(By.cssSelector("option[value='/kategoria/elektronika']"));
+        eleSelect.click();
+        WebElement szukajButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        szukajButton.click();
+        List<WebElement> a = driver.findElements(By.cssSelector("article[data-analytics-enabled='true']"));
 
-        driver.quit();
-    }
-
-    public static void Search(String categoryName, String searchString)
-    {
-        SelectCategory(categoryName);
-        WebElement searchBox = driver.findElement(By.cssSelector("input[name='string']"));
-        searchBox.sendKeys(searchString);
-        searchBox.submit();
-    }
-    public static void SelectCategory(String categoryName)
-    {
-        By categoryDropdownLocator = By.cssSelector("select[aria-label='Kategoria i opcje wyszukiwania']");
-        new Select(driver.findElement(categoryDropdownLocator)).selectByVisibleText(categoryName);
+        for (WebElement element : a) {
+            System.out.println(element.getText());
+            System.out.println("---------");
+        }
     }
 }
+
+
+
+
+
 
 
